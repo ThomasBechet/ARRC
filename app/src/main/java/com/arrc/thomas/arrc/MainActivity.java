797fb42637a300cac698257f2451e9b395b61fb9
terbah <dorian.terb@gmail.com>
2018-10-10 16:23:32 +0200

    Modify Home

--- app/src/main/java/com/arrc/thomas/arrc/MainActivity.java
+++ app/src/main/java/com/arrc/thomas/arrc/MainActivity.java
@@ -1,23 +1,28 @@
 package com.arrc.thomas.arrc;
 
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final long LAUNCH_DELAY = 10000L;
+    private long launch_delay = 10000L;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        ProgressBar spinner =  findViewById(R.id.progressBar);
+        spinner.getIndeterminateDrawable().setColorFilter(Color.BLUE, android.graphics.PorterDuff.Mode.MULTIPLY);
+
         Handler handler = new Handler();
         handler.postDelayed(new RunnableExtend(this){
            @Override
-            public void run() {
-                this.getActivity().setContentView(R.layout.principal_layout);
+            public void run(){
+               this.getActivity().setContentView(R.layout.principal_layout);
            }
-        }, LAUNCH_DELAY);
+        }, launch_delay);
 
     }
 }
