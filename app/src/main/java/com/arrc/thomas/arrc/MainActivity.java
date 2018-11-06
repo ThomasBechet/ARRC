1614999c825d54663be34017adafe2768a92425d
ThomasBechet <thomas.bechet.pro@gmail.com>
2018-11-06 16:27:58 +0100

    Adding EasyAR library

--- app/src/main/java/com/arrc/thomas/arrc/MainActivity.java
+++ app/src/main/java/com/arrc/thomas/arrc/MainActivity.java
@@ -2,12 +2,27 @@ package com.arrc.thomas.arrc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import cn.easyar.Engine;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * License key generated from thomas77 account
+     * DO NOT MODIFY THIS KEY
+     */
+    private static String key = "1GvxUg2oCdsmZ66Jq5gXN56mxRtwEC3gsGrhIo2XUwnIbxBkM3ABzBVsLAggxUlRrL7Ax5d5MALi7v7CqqcImZyMrE3j8WCoAAdpmsJl5HfTxzGY4BG0W1rXxjl4kmE1p1VobmbPw0Kdwut79Iirs9ApkNLxFDDRbPvOBX2UWNhp3VZsEiqc4aXbbiIUt9sKErcAMccm";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        if(Engine.initialize(this, key)){
+            Log.e("THOMAS", "Success");
+        } else{
+            Log.e("THOMAS", "Failed");
+        }
     }
 }
